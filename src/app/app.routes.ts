714125f364266664e41f1f89fdf13c904c68ba5e
Andrew Y Hodsdon <urplelite@devbox.dirtybox>
2025-02-02 04:12:40 -0500

    refactored and restored

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '' , component: HomeComponent},
+{path: 'about' , component: AboutComponent},
+{path: 'services' , component: ServicesComponent},
+{path: 'gallery' , component: GalleryComponent},
+];
