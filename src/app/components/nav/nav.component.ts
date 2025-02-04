15e6aedf702f4dd7f797c0a4df4e7f181e6b1321
Andrew Y Hodsdon <urplelite@devbox.dirtybox>
2025-02-04 08:05:33 -0500

    material imports working

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+//import { MaterialModule } from '../../material.module';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-nav',
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+//import { MaterialModule } from '../../material.module';
+import { MatCardModule } from '@angular/material/card';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
