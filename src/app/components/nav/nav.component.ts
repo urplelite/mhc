cc99799fb8ac3fe6a81df82c5588ec6df254df5a
Andrew Y Hodsdon <urplelite@devbox.dirtybox>
2025-02-04 19:07:32 -0500

    added packages for bootstrap, added ng bootstrap as well, we installed the angular flex-layout and a few others.  I am commiting now and we wil do testing as part of layout before merging with main.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 //import { MaterialModule } from '../../material.module';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+
 @Component({
   selector: 'app-nav',
-  imports: [RouterModule, MatButtonModule],
+  imports: [RouterModule, MatButtonModule, MatIconModule, MatToolbarModule, MatMenuModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
